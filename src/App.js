bb966797170cf26d96dc55995392d5c85dd66902
Yevhenii Chekanskiy <echekanskiy@gmail.com>
2022-01-09 14:26:05 +0200

    Add routing support.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
 import './App.css';
 import {AppBar, IconButton, ToggleButton, ToggleButtonGroup, Toolbar} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import {MapContainer} from "./Map/MapContainer";
 import Box from "@mui/material/Box";
+import {Navigate, Route, Routes, useLocation, useNavigate} from "react-router-dom";
 
-export default class App extends React.Component {
-    render = () => (
-        <Box className="App" sx={{
+export const App = () => {
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const tabSelector = () => {
+        if(location) {
+            return location.pathname
+        } else {
+            return ""
+        }
+    }
+
+    const handleRouteChange = (evt, newRoute) => {
+        navigate(newRoute)
+    }
+
+    return <Box className="App" sx={{
             height: "100%",
             display: "flex",
             minWidth: "500px",
             flexDirection: "column"
         }}>
             <AppBar position="static" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}} className="Navbar">
-                <Toolbar variant="dense" >
+                <Toolbar variant="dense">
                     <IconButton
                         size="large"
                         edge="start"
@@ -26,15 +40,23 @@ export default class App extends React.Component {
                     </IconButton>
                     <ToggleButtonGroup
                         color="primary"
-                        value={"map"}
+                        value={tabSelector()}
+                        onChange={handleRouteChange}
                         exclusive
                     >
-                        <ToggleButton value="map">Map</ToggleButton>
-                        <ToggleButton value="database">Database</ToggleButton>
+                        <ToggleButton value="/map">Map</ToggleButton>
+                        <ToggleButton value="/database">Database</ToggleButton>
                     </ToggleButtonGroup>
                 </Toolbar>
             </AppBar>
-            <MapContainer mapId="main"/>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Navigate to="/map" />}
+                />
+                <Route path="map" element={<MapContainer mapId="main"/>}/>
+                <Route path="database" element={<div>database page</div>}/>
+            </Routes>
+
         </Box>
-    );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {App} from './App';
 import {createTheme, ThemeProvider} from "@mui/material";
 import ModalProvider from 'mui-modal-provider';
 import {RecoilRoot} from "recoil";
 import {loadCanvasKit, CanvasKitProvider} from "./Misc/KitCanvas";
+import {BrowserRouter} from "react-router-dom";
 
 const darkTheme = createTheme({
     palette: {
@@ -17,15 +18,19 @@ const darkTheme = createTheme({
 loadCanvasKit((CanvasKit) => {
     ReactDOM.render(
         <React.StrictMode>
-            <CanvasKitProvider value={CanvasKit}>
-                <RecoilRoot>
-                    <ThemeProvider theme={darkTheme}>
-                        <ModalProvider>
-                            <App/>
-                        </ModalProvider>
-                    </ThemeProvider>
-                </RecoilRoot>
-            </CanvasKitProvider>
+            <RecoilRoot>
+                <BrowserRouter>
+                    <CanvasKitProvider value={CanvasKit}>
+
+                        <ThemeProvider theme={darkTheme}>
+                            <ModalProvider>
+                                <App/>
+                            </ModalProvider>
+                        </ThemeProvider>
+
+                    </CanvasKitProvider>
+                </BrowserRouter>
+            </RecoilRoot>
         </React.StrictMode>,
         document.getElementById('root')
     );

c594fea6ba0408c248a45ee6b05aaf33b0ae9cc9
Yevhenii Chekanskiy <echekanskiy@gmail.com>
2022-04-02 15:44:47 +0300

    [...] remove canvaskit dependency.

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,4 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const path = require('path')
 
 module.exports = function override(config, env) {
     if (!config.plugins) {
@@ -24,30 +23,9 @@ module.exports = function override(config, env) {
         }
     }
 
-    const wasmExtensionRegExp = /\.wasm$/;
-
-    config.resolve.extensions.push('.wasm');
-
-    config.module.rules.forEach(rule => {
-        (rule.oneOf || []).forEach(oneOf => {
-            if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
-                // make file-loader ignore WASM files
-                oneOf.exclude.push(wasmExtensionRegExp);
-            }
-        });
-    });
-
-    // add a dedicated loader for WASM
-    config.module.rules.push({
-        test: wasmExtensionRegExp,
-        include: path.resolve(__dirname, 'src'),
-        use: [{loader: require.resolve('wasm-loader'), options: {}}]
-    });
-
     config.plugins.push(
         new CopyWebpackPlugin({
             patterns: [
-                {from: "node_modules/canvaskit-wasm/bin/canvaskit.wasm", to: "canvaskit.wasm"},
                 {from: "src/Data/database.en.items.json", to: "data/database.en.items.json"},
                 {from: "src/Data/database.en.pins.json", to: "data/database.en.pins.json"},
                 {from: "src/Data/database.ru.items.json", to: "data/database.ru.items.json"},
